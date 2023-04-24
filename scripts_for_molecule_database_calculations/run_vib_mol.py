252f92f99b41ee1f9ca4e21b8cc11a4ffc814024
ThorKongstad <thorkongstadmadsn@hotmail.com>
2023-04-24 12:46:43 +0200

    replaced os system with subprocces

--- scripts_for_molecule_database_calculations/run_vib_mol.py
+++ scripts_for_molecule_database_calculations/run_vib_mol.py
@@ -5,6 +5,7 @@
 
 import argparse
 import os
+from subprocess import call
 import ase.db as db
 from ase.vibrations import Vibrations
 from gpaw import GPAW, PW, Davidson
@@ -13,18 +14,19 @@ from typing import NoReturn
 from ase.parallel import parprint, world
 
 
-def folder_exist(folder_name: str, path:str = '.') -> NoReturn:
-    if folder_name not in os.listdir(path): os.mkdir(ends_with(path,'/')+folder_name)
+def folder_exist(folder_name: str, path: str = '.') -> NoReturn:
+    if folder_name not in os.listdir(path): os.mkdir(ends_with(path, '/')+folder_name)
 
-def file_nexist(file_name: str, path:str = '.', rm_flags='',return_path:bool=False) -> NoReturn|str:
-    if file_name not in os.listdir(path):
-        os.system(f'rm {rm_flags} "{ends_with(path,"/")}{file_name}"')
-        os.wait()
+
+def file_dont_exist(file_name: str, path: str = '.', rm_flags='', return_path: bool = False) -> NoReturn | str:
+    if file_name in os.listdir(path):
+        call(f'rm {rm_flags} "{ends_with(path,"/")}{file_name}"')
     if return_path: return f'{ends_with(path,"/")}{file_name}'
 
-def ends_with(st:str, end:str):
-    if st[-1] != end: return st+end
-    else: return st
+
+def ends_with(string: str, end_str: str) -> str:
+    return string + end_str * (end_str != string[-len(end_str):0])
+
 
 def sanitize(unclean_str: str) -> str:
     for ch in ['!', '*', '?', '{', '[', '(', ')', ']', '}',"'",'"']: unclean_str = unclean_str.replace(ch, '')
@@ -32,18 +34,18 @@ def sanitize(unclean_str: str) -> str:
     for ch in ['=', '+', ':']: unclean_str = unclean_str.replace(ch, '-')
     return unclean_str
 
-def clean_old_files(db_id,smile,functional_folder):
-    os.getcwd()
-    if (old_fil := f'vib_{smile}_{db_id}.txt') in os.listdir(functional_folder):
-        folder_exist(f'old_vibs',path=functional_folder)
-        os.system(f'mv -f "{ends_with(functional_folder,"/")}{old_fil}" "{file_nexist(old_fil,f"{functional_folder}/old_vibs",return_path=True)}"')
-        os.wait()
-    if (old_folder := f'vib_{smile}_{db_id}') in os.listdir(functional_folder):
-        folder_exist(f'old_vibs',path=functional_folder)
-        os.system(f'mv -f "{ends_with(functional_folder,"/")}{old_folder}" {file_nexist(old_folder,f"{functional_folder}/old_vibs",rm_flags="-r",return_path=True)}')
-        os.wait()
 
-def main(db_id:int, clean_old:bool=True, db_dir: str = 'molreact.db'):
+def clean_old_files(functional_folder, file_name):
+    os.getcwd()
+    if file_name in os.listdir(functional_folder):
+        folder_exist(f'old_vibs',path=functional_folder)
+        call(f'mv -f "{ends_with(functional_folder,"/")}{file_name}" "{file_dont_exist(file_name, f"{functional_folder}/old_vibs", return_path=True)}"')
+    if (old_folder := file_name.replace('.txt','')) in os.listdir(functional_folder):
+        folder_exist(f'old_vibs',path=functional_folder)
+        call(f'mv -f "{ends_with(functional_folder,"/")}{old_folder}" {file_dont_exist(old_folder, f"{functional_folder}/old_vibs", rm_flags="-r", return_path=True)}')
+
+
+def main(db_id: int, clean_old: bool = True, db_dir: str = 'molreact.db'):
     # read from  database
     if not os.path.basename(db_dir) in os.listdir(db_path if len(db_path := os.path.dirname(db_dir))>0 else '.'): raise FileNotFoundError("Can't find database")
     with db.connect(db_dir) as db_obj:
@@ -62,16 +64,16 @@ def main(db_id:int, clean_old:bool=True, db_dir: str = 'molreact.db'):
 
     functional_folder = sanitize(functional)
     folder_exist(functional_folder)
+    file_name = f'vib_{smile}_{db_id}.txt'
 
-
-    if world.rank == 0 and clean_old: clean_old_files(db_id,smile,functional_folder)
+    if world.rank == 0 and clean_old: clean_old_files(functional_folder, file_name)
 
     if '{' in functional[0] and '}' in functional[-1] and ':' in functional: functional = eval(functional)
 
     calc = GPAW(mode=PW(500),
                 xc=functional,
                 basis='dzp',
-                txt=f'{functional_folder}/vib_{smile}_{db_id}.txt',
+                txt=f'{functional_folder}/{file_name}',
                 gpts=h2gpts(grid_spacing, atoms.get_cell(), idiv=4),
                 parallel={'augment_grids': True, 'sl_auto': True},
                 convergence={'eigenstates': 0.000001},
@@ -82,14 +84,14 @@ def main(db_id:int, clean_old:bool=True, db_dir: str = 'molreact.db'):
 
     atoms.set_calculator(calc)
     # run vib calc
-    vib = Vibrations(atoms,name=f'{functional_folder}/vib_{smile}_{db_id}')
+    vib = Vibrations(atoms,name=f'{functional_folder}/{file_name.replace(".txt","") }')
     vib.run()
     #vib_dat = vib.get_vibrations()
 
     if world.rank == 0:
-        vib.summary(log=f'{functional_folder}/vib_en_{smile}_{db_id}.txt')
+        vib.summary(log=f'{functional_folder}/{file_name}')
 
-        with open(f'{functional_folder}/vib_en_{smile}_{db_id}.txt','r') as fil:
+        with open(f'{functional_folder}/{file_name.replace("vib","vib_en") }', 'r') as fil:
             energy_string = fil.read()
 
         # saving vib data

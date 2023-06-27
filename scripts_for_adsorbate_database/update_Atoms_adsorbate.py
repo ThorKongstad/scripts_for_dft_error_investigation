657e1738935337e63762c10e8c83e5ee8c0a4871
ThorKongstad <thorkongstadmadsn@hotmail.com>
2023-06-27 09:15:59 +0200

    updated for constraints

--- scripts_for_adsorbate_database/update_Atoms_adsorbate.py
+++ scripts_for_adsorbate_database/update_Atoms_adsorbate.py
@@ -5,10 +5,15 @@ sys.path.insert(0, str(pathlib.Path(__file__).parent.parent))
 from scripts_for_adsorbate_database import update_db
 from ase.io import read
 from ase import Atoms
+import ase.db as db
 
 
-def main(db_index: int, txt_dir: str, db_dir: str, relaxed: bool = False):
+def main(db_index: int, txt_dir: str, db_dir: str, relaxed: bool = False, discard_constraint: bool = False):
+    if discard_constraint:
+        old_atoms: Atoms = read(db_dir,index=db_index-1)
+        constraints = old_atoms.constraints
     updated_atoms: Atoms = read(txt_dir, index=-1)
+    if discard_constraint: updated_atoms.set_constraint(constraints)
     update_db(db_dir,
               dict(
                   id=db_index,
@@ -25,6 +30,7 @@ if __name__ == '__main__':
     parser.add_argument('txt_directory', help='Path to the gpaw output.')
     parser.add_argument('db_directory', help='Path to the database')
     parser.add_argument('-relaxed', '--relaxed', action='store_true', default=False)
+    parser.add_argument('-disCon', '--discard_constraint', action='store_true', default=False)
     args = parser.parse_args()
 
     main(args.db_index, args.txt_directory, args.db_directory, args.relaxed)

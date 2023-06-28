36572a4011c61a4f832cfa19e172b2382a7ce63d
ThorKongstad <thorkongstadmadsn@hotmail.com>
2023-06-28 09:44:54 +0200

    updated for constraints

--- scripts_for_adsorbate_database/update_Atoms_adsorbate.py
+++ scripts_for_adsorbate_database/update_Atoms_adsorbate.py
@@ -7,12 +7,12 @@ from ase.io import read
 from ase import Atoms
 
 
-def main(db_index: int, txt_dir: str, db_dir: str, relaxed: bool = False, discard_constraint: bool = False):
-    if discard_constraint:
+def main(db_index: int, txt_dir: str, db_dir: str, relaxed: bool = False, new_constraint: bool = False):
+    if not new_constraint:
         old_atoms: Atoms = read(db_dir,index=db_index-1)
         constraints = old_atoms.constraints
     updated_atoms: Atoms = read(txt_dir, index=-1)
-    if discard_constraint: updated_atoms.set_constraint(constraints)
+    if not new_constraint: updated_atoms.set_constraint(constraints)
     update_db(db_dir,
               dict(
                   id=db_index,
@@ -29,7 +29,7 @@ if __name__ == '__main__':
     parser.add_argument('txt_directory', help='Path to the gpaw output.')
     parser.add_argument('db_directory', help='Path to the database')
     parser.add_argument('-relaxed', '--relaxed', action='store_true', default=False)
-    parser.add_argument('-disCon', '--discard_constraint', action='store_true', default=False)
+    parser.add_argument('-newCon', '--new_constraint', action='store_true', default=False)
     args = parser.parse_args()
 
-    main(args.db_index, args.txt_directory, args.db_directory, args.relaxed, args.discard_constraint)
+    main(args.db_index, args.txt_directory, args.db_directory, args.relaxed, args.new_constraint)

af641312b87a34c75be7c42fb6393ff22e468405
ThorKongstad <thorkongstadmadsn@hotmail.com>
2023-11-10 11:21:48 +0100

    axis update

--- scripts_for_adsorbate_database/vulcano.py
+++ scripts_for_adsorbate_database/vulcano.py
@@ -95,7 +95,8 @@ def vulcano_plotly(functional_list: Sequence[Functional], oh_reactions: Sequence
                 except: pass
 
     fig.update_layout(
-        xaxis_title='OH adsorption',
+        title='ORR',
+        xaxis_title='OH adsorption energy',
         yaxis_title='Overpotential'
     )
 
@@ -112,8 +113,8 @@ def main(slab_db_dir: list[str], adsorbate_db_dir: list[str], mol_db_dir: list[s
 
     functional_set = {xc for _, row in pd_adsorbate_dat.iterrows() if not pd.isna((xc := row.get('xc')))}
 
-    oh_ad_h2_water = adsorption_OH_reactions#[1::3] #[1,4,7,10,13,16]
-    ooh_ad_h2_water = adsorption_OOH_reactions#[1::3]
+    oh_ad_h2_water = adsorption_OH_reactions[1::3] #[1,4,7,10,13,16]
+    ooh_ad_h2_water = adsorption_OOH_reactions[1::3]
 
     dictionary_of_needed_strucs = {'molecule': [], 'slab': [], 'adsorbate': []}
     for reac in oh_ad_h2_water + ooh_ad_h2_water:

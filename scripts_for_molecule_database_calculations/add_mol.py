58d65ad9755a7231490ca64efcae0e1a884763da
ThorKongstad <thorkongstadmadsn@hotmail.com>
2023-11-13 12:45:40 +0100

    beautification

--- scripts_for_molecule_database_calculations/add_mol.py
+++ scripts_for_molecule_database_calculations/add_mol.py
@@ -1,5 +1,11 @@
 import argparse
 import os
+import sys
+import pathlib
+
+sys.path.insert(0, str(pathlib.Path(__file__).parent.parent))
+from scripts_for_molecule_database_calculations import sanitize, folder_exist
+
 # from ase.io import *
 import ase.db as db
 from ase.data.pubchem import pubchem_atoms_search
@@ -8,24 +14,12 @@ from gpaw.cluster import Cluster
 # from gpaw import GPAW, PW, Davidson
 # from gpaw.utilities import h2gpts
 # import numpy as np
-from typing import NoReturn
 
 
-def folder_exist(folder_name: str) -> NoReturn:
-    if folder_name not in os.listdir(): os.mkdir(folder_name)
-
-
-def sanitize(unclean_str: str) -> str:
-    for ch in ['!', '*', '?', '{', '[', '(', ')', ']', '}', "'",'"']: unclean_str = unclean_str.replace(ch, '')
-    for ch in ['/', '\\', '|',' ']: unclean_str = unclean_str.replace(ch, '_')
-    for ch in ['=', '+', ':']: unclean_str = unclean_str.replace(ch, '-')
-    return unclean_str
-
-
-def main(smile: str, functional: str, db_dir: str = 'molreact.db', setup_path: str | bool = False, grid_spacing: float = 0.16) -> NoReturn:
+def main(smile: str, functional: str, db_dir: str = 'molreact.db', setup_path: str | bool = False, grid_spacing: float = 0.16):
     # create ase mol
     if smile == '[HH]': atoms = Cluster(molecule('H2'))  # pubchem search hates hydrogen, it hates its name, it hates its cid and most of all it hates its weird smile and dont you dare confuse HH for hydrogen
-    elif 'cid' in smile: atoms = Cluster(pubchem_atoms_search(cid=int(smile.replace('cid',''))))
+    elif 'cid' in smile: atoms = Cluster(pubchem_atoms_search(cid=int(smile.replace('cid', ''))))
     else: atoms = Cluster(pubchem_atoms_search(smiles=smile))
     atoms.minimal_box(border=6, h=grid_spacing, multiple=4)
 
@@ -39,8 +33,8 @@ def main(smile: str, functional: str, db_dir: str = 'molreact.db', setup_path: s
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('smiles_str')
-    parser.add_argument('functional',help='str denoting what fucntional to calculate with')
-    parser.add_argument('-db','--database',help='name or directory for the database, if not stated will make a molreact.db in pwd.', default='molreact.db')
+    parser.add_argument('functional', help='str denoting what fucntional to calculate with')
+    parser.add_argument('-db', '--database', help='name or directory for the database, if not stated will make a molreact.db in pwd.', default='molreact.db')
     parser.add_argument('--setup', '-s', default=False)
     parser.add_argument('--grid_spacing', '-g', default=0.16)
     args = parser.parse_args()

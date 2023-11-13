b51d72b2eb7139514f5a83d108ef22001c161731
ThorKongstad <thorkongstadmadsn@hotmail.com>
2023-11-13 11:24:04 +0100

    changed to pt reference

--- scripts_for_adsorbate_database/vulcano.py
+++ scripts_for_adsorbate_database/vulcano.py
@@ -101,7 +101,7 @@ def vulcano_plotly(functional_list: Sequence[Functional], oh_reactions: Sequence
     )
 
     folder_exist('reaction_plots')
-    save_name = 'reaction_plots/vulcano_plot'
+    save_name = 'reaction_plots/vulcano_pt_ref_plot'
     if png_bool: fig.write_image(save_name + '.png')
     fig.write_html(save_name + '.html', include_mathjax='cdn')
 
@@ -114,7 +114,7 @@ def main(slab_db_dir: list[str], adsorbate_db_dir: list[str], mol_db_dir: list[s
     functional_set = {xc for _, row in pd_adsorbate_dat.iterrows() if not pd.isna((xc := row.get('xc')))}
 
     oh_ad_h2_water = metal_ref_ractions[0::2] #adsorption_OH_reactions[1::3] #[1,4,7,10,13,16]
-    ooh_ad_h2_water = metal_ref_ractions[0::2] #adsorption_OOH_reactions[1::3]
+    ooh_ad_h2_water = metal_ref_ractions[1::2] #adsorption_OOH_reactions[1::3]
 
     dictionary_of_needed_strucs = {'molecule': [], 'slab': [], 'adsorbate': []}
     for reac in oh_ad_h2_water + ooh_ad_h2_water:

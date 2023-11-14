141de7b0d9938d648843125da76939de0a44e490
ThorKongstad <thorkongstadmadsn@hotmail.com>
2023-11-14 10:51:12 +0100

    raise errors if db arent present

--- scripts_for_molecule_database_calculations/__init__.py
+++ scripts_for_molecule_database_calculations/__init__.py
@@ -22,6 +22,9 @@ class reaction:
 
 def build_pd(db_dir_list, select_key: Optional = None):
     if isinstance(db_dir_list, str): db_dir_list = [db_dir_list]
+    for db_dir in db_dir_list:
+        if not os.path.basename(db_dir) in os.listdir(db_path if len(db_path := os.path.dirname(db_dir)) > 0 else '.'):
+            raise FileNotFoundError("Can't find database")
     db_list = [db.connect(work_db) for work_db in db_dir_list]
     pd_dat = pd.DataFrame([row.__dict__ for work_db in db_list for row in work_db.select(selection=select_key)])
     return pd_dat

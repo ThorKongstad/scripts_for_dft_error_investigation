5816a3e40a85305c4c8bcd5138a6296f0d230fa4
ThorKongstad <thorkongstadmadsn@hotmail.com>
2024-01-19 11:26:08 +0100

    updated water correction

---
+++ scripts_for_adsorbate_database/remove_H.py
@@ -0,0 +1,24 @@
+import argparse
+from copy import deepcopy
+
+from ase.io import read, write
+from ase import Atoms
+
+
+def main(original_struc, out_trajectory_name):
+    old_atoms: Atoms = read(original_struc)
+    constraints = deepcopy(old_atoms.constraints)
+    old_atoms.set_constraint()
+
+    del old_atoms[[at.index for at in old_atoms if at.symbol == 'H']]
+
+    old_atoms.set_constraint(constraints)
+
+    write(out_trajectory_name, old_atoms)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('struc_source')
+    parser.add_argument('out_traj')
+    args = parser.parse_args()

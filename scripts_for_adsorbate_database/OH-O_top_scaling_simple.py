a62fb8f649167f298b1e2413493538fabfd2a140
ThorKongstad <thorkongstadmadsn@hotmail.com>
2024-01-22 12:29:22 +0100

    O top scalling

--- scripts_for_adsorbate_database/OH-O_top_scaling_simple.py
+++ scripts_for_adsorbate_database/OH-O_top_scaling_simple.py
@@ -104,7 +104,7 @@ def scaling_plot(functional_list: Sequence[Functional], oh_reactions: Sequence[a
     )
 
     folder_exist('reaction_plots')
-    save_name = 'reaction_plots/scaling_plot_OH_O_simple'
+    save_name = 'reaction_plots/scaling_plot_OH_O_top_simple'
     if png_bool: fig.write_image(save_name + '.png')
     fig.write_html(save_name + '.html', include_mathjax='cdn')
 

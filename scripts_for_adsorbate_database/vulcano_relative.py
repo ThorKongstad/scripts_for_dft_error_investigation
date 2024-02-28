15d7fba8368b217e7690e6f7e77b7dcf9b80061a
ThorKongstad <thorkongstadmadsn@hotmail.com>
2024-02-28 10:07:49 +0100

    relative to defined distance.

--- scripts_for_adsorbate_database/vulcano_relative.py
+++ scripts_for_adsorbate_database/vulcano_relative.py
@@ -8,7 +8,7 @@ from re import match
 from dataclasses import dataclass, field
 
 sys.path.insert(0, str(pathlib.Path(__file__).parent.parent))
-from scripts_for_adsorbate_database import sanitize, folder_exist, build_pd, adsorbate_reaction, adsorption_OH_reactions, adsorption_OOH_reactions, metal_ref_ractions, adsorption_O_reactions, sd
+from scripts_for_adsorbate_database import sanitize, folder_exist, build_pd, adsorbate_reaction, adsorption_OH_reactions, adsorption_OOH_reactions, metal_ref_ractions, adsorption_O_reactions, sd, mean
 from scripts_for_adsorbate_database.adsorbate_correlation_plot import Functional
 
 import numpy as np
@@ -16,6 +16,23 @@ import pandas as pd
 from scipy import stats, odr
 import plotly.graph_objects as go
 import plotly.express as px
+
+
+def ode_1par_linear(reac_1_Energies: Sequence[float], reac_2_Energies: Sequence[float], reac_1_Energies_sigma: Optional[Sequence[float]] = None, reac_2_Energies_sigma: Optional[Sequence[float]] = None) -> 'odr_linear_fitting_results':
+    @dataclass
+    class odr_linear_fitting_results:
+        slope: float
+        stderr: float
+        intercept: float
+        intercept_stderr: float
+
+    fitting_model = odr.Model(lambda beta, x: x+beta[0])
+
+    fit_odr_dat = odr.Data(x=reac_1_Energies, wd=(1 / (np.power(reac_1_Energies_sigma, 2))) if reac_1_Energies_sigma is not None else None,
+                           y=reac_2_Energies, we=(1 / (np.power(reac_2_Energies_sigma, 2))) if reac_1_Energies_sigma is not None else None)
+    odr_fit_obj = odr.ODR(fit_odr_dat, fitting_model, beta0=[3.2]).run()  # beta0 is the initial guess for the scalling relation
+    fit_result = odr_linear_fitting_results(slope=1, intercept=odr_fit_obj.beta[0], stderr=0, intercept_stderr=odr_fit_obj.sd_beta[0])
+    return fit_result
 
 
 def scaling_fit(reac_1_Energies: Sequence[float], reac_2_Energies: Sequence[float], reac_1_Energies_sigma: Optional[Sequence[float]] = None, reac_2_Energies_sigma: Optional[Sequence[float]] = None):
@@ -43,7 +60,7 @@ def Linear_func(x: float, a: float, b: float) -> float: return a*x+b
 def overpotential(dG_OOH: float, dG_OH: float, dG_O: float) -> float: return min((4.92 - dG_OOH, dG_OOH - dG_O, dG_O - dG_OH, dG_OH)) # 1.23 -
 
 
-def vulcano_plotly(functional_list: Sequence[Functional], oh_reactions: Sequence[adsorbate_reaction], ooh_reactions: Sequence[adsorbate_reaction], o_reactions: Sequence[adsorbate_reaction], oh_reaction_relative: adsorbate_reaction, ooh_reactions_relative: adsorbate_reaction, o_reactions_relative: adsorbate_reaction,  png_bool: bool = False):
+def vulcano_plotly(functional_list: Sequence[Functional], oh_reactions: Sequence[adsorbate_reaction], ooh_reactions: Sequence[adsorbate_reaction], o_reactions: Sequence[adsorbate_reaction], oh_reaction_relative: Sequence[adsorbate_reaction], ooh_reactions_relative: Sequence[adsorbate_reaction], o_reactions_relative: Sequence[adsorbate_reaction],  png_bool: bool = False):
     fig = go.Figure()
 
     colour_dict_functional = {
@@ -87,88 +104,121 @@ def vulcano_plotly(functional_list: Sequence[Functional], oh_reactions: Sequence
 #      showlegend=False,
 #    ))
 
+    OH_corr = 0.35 - 0.5 # + 0.35 is dZPE - TdS from 10.1021/jp047349j, - 0.5 is water stability correction 10.1021/cs300227s
+    OOH_corr = 0.4 - 0.3
+
     beef = [xc for xc in functional_list if xc.name == 'BEEF-vdW'][0]
 
-    OH_OOH_scalling_fit = scaling_fit([beef.calculate_reaction_enthalpy(OH_reac) for OH_reac in oh_reactions],
-                                      [beef.calculate_reaction_enthalpy(OOH_reac) for OOH_reac in ooh_reactions],
-                                      [sd(beef.calculate_BEE_reaction_enthalpy(OH_reac)) for OH_reac in oh_reactions],
-                                      [sd(beef.calculate_BEE_reaction_enthalpy(OOH_reac)) for OOH_reac in ooh_reactions])
+    OH_OOH_scalling_fit = ode_1par_linear([beef.calculate_reaction_enthalpy(OH_reac) + OH_corr for OH_reac in oh_reactions],
+                                      [beef.calculate_reaction_enthalpy(OOH_reac) + OOH_corr for OOH_reac in ooh_reactions],
+                                      [sd(beef.calculate_BEE_reaction_enthalpy(OH_reac) + OH_corr) for OH_reac in oh_reactions],
+                                      [sd(beef.calculate_BEE_reaction_enthalpy(OOH_reac) + OOH_corr) for OOH_reac in ooh_reactions])
+
+    oh_ensamble = list(map(lambda reac: beef.calculate_BEE_reaction_enthalpy(reac) + OH_corr, oh_reactions)) # is a nested matrix like object, with rows corresponding the metals and col as each ensamble function
+    ooh_ensamble = list(map(lambda reac: beef.calculate_BEE_reaction_enthalpy(reac) + OOH_corr, ooh_reactions))
+    fit_ens_objs = [ode_1par_linear(reac_1_Energies=OH_vals, reac_2_Energies=OOH_vals) for OH_vals, OOH_vals in zip(zip(*oh_ensamble), zip(*ooh_ensamble))]#[stats.linregress(x=OH_vals, y=OOH_vals) for OH_vals, OOH_vals in zip(zip(*oh_ensamble), zip(*ooh_ensamble))]
+    intercept_ens = np.array(fit.intercept for fit in fit_ens_objs)
+
+    beta_G_mean = mean(tuple(fit.intercept for fit in fit_ens_objs))
+    beta_G_SD = sd(tuple(fit.intercept for fit in fit_ens_objs), beta_G_mean)
+
+    volcano_peak_ens = (4.92 - intercept_ens)/2
+    volcano_peak_mean = (4.92 - beta_G_mean) / 2
+    volcano_peak_sd = beta_G_SD/2
+
+    for ens_peak in volcano_peak_ens:
+        fig.add_vline(
+            x=ens_peak,
+            line_color='#0000CD',
+            opacity=0.2
+        )
 
     fig.add_vline(
-        x=0.11,
+        x=volcano_peak_mean,
         line_dash='dash',
     )
 
     fig.add_vrect(
-        x0=0.11 - (top_stderr:=OH_OOH_scalling_fit.intercept_stderr / 2), x1=0.11 + OH_OOH_scalling_fit.intercept_stderr / 2,
+        x0=volcano_peak_mean - volcano_peak_sd, x1=0.11 + volcano_peak_sd,
         fillcolor="green",
         opacity=0.25,
-        annotation_text=f"Vulcano top location <br> stderr: {top_stderr}", annotation_position="top left"
+        annotation_text=f"Vulcano top location <br> stderr: {volcano_peak_sd:.3f} eV", annotation_position="top left"
     )
 
+    beef_pt_OH = volcano_peak_mean - 0.11
+    fig.add_trace(go.Scatter(
+        mode='markers',
+        name=f'BEEF-vdW-Pt',
+        x=[beef_pt_OH],
+        y=[beef_pt_OH],
+        hovertemplate=f'functional: BEEF-vdW' + '<br>' + f'metal: Pt' + '<br>',
+        error_x=dict(type='constant', value=volcano_peak_sd,
+                     color=colour_dict_metal['Pt'], thickness=1.5,
+                     width=3, visible=False),
+        legendgroup='Pt',
+        legendgrouptitle_text='Pt',
+        marker=dict(size=16, color=colour_dict_metal['Pt'], symbol=marker_dict_functional['BEEF-vdW'])
+    ))
 
-    for oh_reac, ooh_reac, o_reac in zip(oh_reactions, ooh_reactions, o_reactions):
+    fig.add_trace(go.Scatter(
+        mode='markers',
+        name=f'BEE for Pt BEEF-vdW',
+        y=volcano_peak_ens - 0.11,
+        x=volcano_peak_ens - 0.11,
+        hovertemplate=f'metal: Pt',
+        marker=dict(color=colour_dict_metal['Pt'], opacity=0.5, ),
+        legendgroup='Pt',
+        legendgrouptitle_text='Pt',
+    ))
+
+    for oh_reac, ooh_reac in zip(oh_reaction_relative, ooh_reactions_relative,):
         assert (metal := oh_reac.products[0].name.split('_')[0]) == ooh_reac.products[0].name.split('_')[0]
-        for xc in functional_list:
-            marker_arg = dict(marker=dict(size=16, color=colour_dict_metal[metal] if metal in colour_dict_metal.keys() else 'DarkSlateGrey', symbol=marker_dict_functional[xc.name] if xc.name in marker_dict_functional.keys() else 'circle'))
-            try: fig.add_trace(go.Scatter(
-                mode='markers',
-                name=f'{xc.name}-{metal}',
-                x=[(oh_adsorp := xc.calculate_reaction_enthalpy(oh_reac)) + 0.35 - 0.5 - ((oh_adsorp_relative := xc.calculate_reaction_enthalpy(oh_reaction_relative)) + 0.35 - 0.5)], # + 0.35 is dZPE - TdS from 10.1021/jp047349j, - 0.3 is water stability correction 10.1021/cs300227s
-                y=[overpotential(
-                    dG_OOH=(ooh_adsorp := xc.calculate_reaction_enthalpy(ooh_reac)) + 0.40 - 0.3,
-                    dG_OH=oh_adsorp + 0.35 - 0.5,
-                    dG_O=xc.calculate_reaction_enthalpy(o_reac) + 0.05 # 0.05 is dZPE - TdS from 10.1021/acssuschemeng.8b04173
-                )
-                - overpotential(
-                    dG_OOH=(xc.calculate_reaction_enthalpy(ooh_reactions_relative)) + 0.40 - 0.3,
-                    dG_OH=oh_adsorp_relative + 0.35 - 0.5,
-                    dG_O=xc.calculate_reaction_enthalpy(o_reactions_relative) + 0.05 # 0.05 is dZPE - TdS from 10.1021/acssuschemeng.8b04173
-                )],
-                hovertemplate=f'functional: {xc.name}' + '<br>' + f'metal: {metal}' + '<br>' + f'OH adsorption: {str(oh_reac)}' + '<br>' + f'OOH adsorption: {str(ooh_reac)}' + '<br>' + f'O adsorption: {str(o_reac)}',
-                legendgroup=metal,
-                legendgrouptitle_text=metal,
-                **marker_arg
-            ))
-            except: traceback.print_exc()
+        xc = beef
+        marker_arg = dict(marker=dict(size=16, color=colour_dict_metal[metal] if metal in colour_dict_metal.keys() else 'DarkSlateGrey', symbol=marker_dict_functional[xc.name] if xc.name in marker_dict_functional.keys() else 'circle'))
+        try: fig.add_trace(go.Scatter(
+            mode='markers',
+            name=f'{xc.name}-{metal}',
+            x=[beef_pt_OH + xc.calculate_reaction_enthalpy(oh_reac)], # + 0.35 is dZPE - TdS from 10.1021/jp047349j, - 0.3 is water stability correction 10.1021/cs300227s
+            y=[overpotential(
+                dG_OOH=(ooh_adsorp := beef_pt_OH + xc.calculate_reaction_enthalpy(oh_reac) + beta_G_mean),
+                dG_OH=beef_pt_OH + xc.calculate_reaction_enthalpy(oh_reac),
+                dG_O=beef_pt_OH + xc.calculate_reaction_enthalpy(oh_reac) * 2 # 0.05 is dZPE - TdS from 10.1021/acssuschemeng.8b04173
+            )],
+            hovertemplate=f'functional: {xc.name}' + '<br>' + f'metal: {metal}' + '<br>' + f'OH adsorption: {str(oh_reac)}' + '<br>' + f'OOH adsorption: {str(ooh_reac)}',
+            legendgroup=metal,
+            legendgrouptitle_text=metal,
+            **marker_arg
+        ))
+        except: traceback.print_exc()
 
-            if xc.name in ['BEEF-vdW', "{'name':'BEEF-vdW','backend':'libvdwxc'}"]:
-                try:
-                    fig.add_trace(go.Scatter(
-                        mode='markers',
-                        name=f'BEE for {metal} {xc.name}',
-                        y=(ens_y_cloud := np.array(list(map(lambda ooh, oh, o: overpotential(
-                                dG_OOH=ooh + 0.40 - 0.3,
-                                dG_OH=oh + 0.35 - 0.5,
-                                dG_O=o + 0.05
-                                ),
-                            xc.calculate_BEE_reaction_enthalpy(ooh_reac).tolist(),
-                            (oh_ensem := xc.calculate_BEE_reaction_enthalpy(oh_reac)).tolist(),
-                            xc.calculate_BEE_reaction_enthalpy(o_reac).tolist()
-                            )))
-                          - np.array(list(map(lambda ooh, oh, o: overpotential(
-                                dG_OOH=ooh + 0.40 - 0.3,
-                                dG_OH=oh + 0.35 - 0.5,
-                                dG_O=o + 0.05
-                                ),
-                            xc.calculate_BEE_reaction_enthalpy(ooh_reactions_relative).tolist(),
-                            (oh_ensem_relative := xc.calculate_BEE_reaction_enthalpy(oh_reaction_relative)).tolist(),
-                            xc.calculate_BEE_reaction_enthalpy(o_reactions_relative).tolist(),
-                            )))),
-                        x=(ens_x_cloud := oh_ensem + 0.35 - 0.5 - (oh_ensem_relative + 0.35 - 0.5)),
-                        hovertemplate=f'metal: {metal}' + '<br>' + f'OH adsorption: {str(oh_reac)}' + '<br>' + f'OOH adsorption: {str(ooh_reac)}' + '<br>' + f'O adsorption: {str(o_reac)}',
-                        marker=dict(color=colour_dict_metal[metal] if metal in colour_dict_metal.keys() else 'Grey', opacity=0.5, ),
-                        legendgroup=metal,
-                        legendgrouptitle_text=metal,
-                    ))
+        if xc.has_BEE:
+            try:
+                fig.add_trace(go.Scatter(
+                    mode='markers',
+                    name=f'BEE for {metal} {xc.name}',
+                    y=(ens_y_cloud := np.array(list(map(lambda ooh, oh, o: overpotential(
+                            dG_OOH=ooh,
+                            dG_OH=oh,
+                            dG_O=o
+                            ),
+                        ((volcano_peak_ens - 0.11) + xc.calculate_BEE_reaction_enthalpy(oh_reac) + intercept_ens).tolist(),
+                        ((volcano_peak_ens - 0.11) + xc.calculate_BEE_reaction_enthalpy(oh_reac)).tolist(),
+                        ((volcano_peak_ens - 0.11) + xc.calculate_BEE_reaction_enthalpy(oh_reac) * 2).tolist()
+                        )))),
+                    x=(ens_x_cloud := (volcano_peak_ens - 0.11) + xc.calculate_BEE_reaction_enthalpy(oh_reac)),
+                    hovertemplate=f'metal: {metal}' + '<br>' + f'OH adsorption: {str(oh_reac)}' + '<br>' + f'OOH adsorption: {str(ooh_reac)}' + '<br>' + f'O adsorption: {str(o_reac)}',
+                    marker=dict(color=colour_dict_metal[metal] if metal in colour_dict_metal.keys() else 'Grey', opacity=0.5, ),
+                    legendgroup=metal,
+                    legendgrouptitle_text=metal,
+                ))
 
-                    fig.update_traces(selector=dict(name=f'{xc.name}-{metal}'),
-                                      error_x=dict(type='constant', value=sd(ens_x_cloud), color=colour_dict_metal[metal] if metal in colour_dict_metal.keys() else 'Grey', thickness=1.5, width=3, visible=False),
-                                      error_y=dict(type='constant', value=sd(ens_y_cloud), color=colour_dict_metal[metal] if metal in colour_dict_metal.keys() else 'Grey', thickness=1.5, width=3, visible=False)
-                                      )
+                fig.update_traces(selector=dict(name=f'{xc.name}-{metal}'),
+                                  error_x=dict(type='constant', value=sd(ens_x_cloud), color=colour_dict_metal[metal] if metal in colour_dict_metal.keys() else 'Grey', thickness=1.5, width=3, visible=False),
+                                  error_y=dict(type='constant', value=sd(ens_y_cloud), color=colour_dict_metal[metal] if metal in colour_dict_metal.keys() else 'Grey', thickness=1.5, width=3, visible=False)
+                                  )
 
-                    fig.data = fig.data[-1:] + fig.data[0:-1]
-                except: pass
+                fig.data = fig.data[-1:] + fig.data[0:-1]
+            except: pass
 
     fig.update_layout(
         title='ORR',
@@ -259,9 +309,9 @@ def main(slab_db_dir: list[str], adsorbate_db_dir: list[str], mol_db_dir: list[s
                    oh_reactions=oh_ad_h2_water,
                    ooh_reactions=ooh_ad_h2_water,
                    o_reactions=o_ad_h2_water,
-                   oh_reaction_relative=oh_ad_h2_water[0],
-                   ooh_reactions_relative=ooh_ad_h2_water[0],
-                   o_reactions_relative=o_ad_h2_water[0])
+                   oh_reaction_relative=oh_ad_h2_water,
+                   ooh_reactions_relative=ooh_ad_h2_water,
+                   o_reactions_relative=o_ad_h2_water)
 
 
 if __name__ == '__main__':

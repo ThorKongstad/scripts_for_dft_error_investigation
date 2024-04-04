567dbaf0433e05d22fbb10aeacdb2daee98b2f9d
ThorKongstad <thorkongstadmadsn@hotmail.com>
2024-04-04 14:52:15 +0200

    added comparisons

--- neb_ensamble_calc.py
+++ neb_ensamble_calc.py
@@ -1,31 +1,48 @@
 #partition=katla_medium
-#nprocshared=8
+#nprocshared=24
 #mem=2300MB
 #constrain='[v4|v5]'
 import argparse
 import os
+from tenacity import retry, retry_if_exception_type, stop_after_attempt, wait_fixed
+from sqlite3 import OperationalError
 import ase.db as db
 import numpy as np
 from ase.io import read
+from ase.optimize import QuasiNewton
 from gpaw import GPAW, PW, Davidson
 from gpaw.utilities import h2gpts
-from typing import NoReturn, Sequence
+from typing import NoReturn, Sequence, Optional
 from ase.parallel import parprint, world
 from ase.dft.bee import BEEFEnsemble
+import time
 
-def folder_exist(folder_name: str) -> NoReturn:
-    if not  os.path.basename(folder_name) in os.listdir(os.path.dirname(folder_name)): os.mkdir(folder_name)
+
+@retry(retry=retry_if_exception_type(FileExistsError), stop=stop_after_attempt(5), wait=wait_fixed(2))
+def folder_exist(folder_name: str, path: str = '.') -> None:
+    if folder_name not in os.listdir(path): os.mkdir(ends_with(path, '/') + folder_name)
+
+
+def ends_with(string: str, end_str: str) -> str:
+    return string + end_str * (end_str != string[-len(end_str):0])
+
 
 def sanitize(unclean_str: str) -> str:
-    for ch in ['!', '*', '?', '{', '[', '(', ')', ']', '}',"'",'"']: unclean_str = unclean_str.replace(ch, '')
-    for ch in ['/', '\\', '|',' ']: unclean_str = unclean_str.replace(ch, '_')
-    for ch in ['=', '+', ':']: unclean_str = unclean_str.replace(ch, '-')
+    for ch in ['!', '*', '?', '{', '[', '(', ')', ']', '}', "'", '"']: unclean_str = unclean_str.replace(ch, '')
+    for ch in ['/', '\\', '|', ' ', ',', '.']: unclean_str = unclean_str.replace(ch, '_')
+    for ch in ['=', '+', ':', ';']: unclean_str = unclean_str.replace(ch, '-')
     return unclean_str
 
-def weighted_mean(dat: Sequence[float|int], coef: Sequence[float|int]) -> float|int:
-    return sum(dat_i*coef_i for dat_i,coef_i in zip(dat,coef))/ sum(coef)
 
-def weighted_sd(dat: Sequence[float|int], coef: Sequence[float|int],mean:float|int) -> float|int:
+def weighted_mean(dat: Sequence[float], coef: Sequence[float]) -> float:
+    return sum(dat_i*coef_i for dat_i, coef_i in zip(dat, coef)) / sum(coef)
+
+
+def mean(values): return sum(values) / len(values)
+def sd(values, mean_value): return np.sqrt(1 / len(values) * sum(((x - mean_value) ** 2 for x in values)))
+
+
+def weighted_sd(dat: Sequence[float], coef: Sequence[float], mean: float) -> float:
     # https://www.itl.nist.gov/div898/software/dataplot/refman2/ch2/weightsd.pdf
     non_zero_coefs = len([co for co in coef if co != 0])
     return np.sqrt(
@@ -33,19 +50,29 @@ def weighted_sd(dat: Sequence[float|int], coef: Sequence[float|int],mean:float|i
         (((non_zero_coefs-1)*sum(coef))/non_zero_coefs)
     )
 
-def main(calc_name:str,structures:Sequence[str]):
+
+@retry(retry=retry_if_exception_type(OperationalError), stop=stop_after_attempt(5), wait=wait_fixed(10))
+def update_db(db_dir: str, db_update_args: dict):
+    with db.connect(db_dir) as db_obj:
+        db_obj.update(**db_update_args)
 
 
-    folder = '/groups/kemi/thorkong/errors_investigation/transistion_calc/' + sanitize(calc_name)
-    folder_exist(folder)
+def main(calc_name: str, structures: Sequence[str], db_name: Optional[str] = None, direc: Optional[str] = None):
+    if direc is None: direc = '/groups/kemi/thorkong/errors_investigation/transistion_calc/'
+    folder = ends_with(direc, '/') + sanitize(calc_name)
+    if world.rank == 0: folder_exist(os.path.basename(folder), path=os.path.dirname(folder))
 
     grid_spacing = 0.16
-    for image_name in structures:
-        image = read(image_name)
+
+    if len(structures) == 1: images = read(structures[0], index=':')
+    else: images = map(read, structures)
+
+    for nr, image in enumerate(images):
         calc = GPAW(mode=PW(500),
-                    xc='RPBE',
+                    kpts=(4, 4, 1),
+                    xc='BEEF-vdW',
                     basis='dzp',
-                    txt=f'{folder}/BEE_{os.path.basename(image_name)}.txt',
+                    txt=f'{folder}/BEE_image-{nr}.txt',
                     gpts=h2gpts(grid_spacing, image.get_cell(), idiv=4),
                     parallel={'augment_grids': True, 'sl_auto': True},
                     convergence={'eigenstates': 0.000001},
@@ -55,22 +82,26 @@ def main(calc_name:str,structures:Sequence[str]):
                     )
 
         image.set_calculator(calc)
-        potential_e = image.get_potential_energy()
+        image.get_potential_energy()
+        image.get_forces()
         ens = BEEFEnsemble(image)
         ensem_en_li = ens.get_ensemble_energies()
-        ensem_coef = ens.get_beefvdw_ensemble_coefs()
-        ensem_mean = weighted_mean(ensem_en_li,ensem_coef)
-        ensem_sd = weighted_sd(ensem_en_li,ensem_coef,ensem_mean)
+        ensem_mean = mean(ensem_en_li)
+        ensem_sd = sd(ensem_en_li, ensem_mean)
 
         if world.rank == 0:
-            with db.connect(f'{folder}/{sanitize(calc_name)}.db') as db_obj:
-                data_dict = {'ensemble_en': ensem_en_li,'ensemble_coef':ensem_coef}
-                db_obj.write(image,ensem_mean = ensem_mean, ensem_sd = ensem_sd, data = data_dict)
+            with db.connect(f'{folder}/{sanitize(db_name if db_name else calc_name)}.db') as db_obj:
+                data_dict = {'ensemble_en': ensem_en_li} #,'ensemble_coef':ensem_coef}
+                db_obj.write(image, name=f'image_{nr}', ensem_mean=ensem_mean, ensem_sd=ensem_sd, data=data_dict)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('calculation_name',type=str)
-    parser.add_argument('files',nargs='+')
+    parser.add_argument('calculation_name', type=str)
+    parser.add_argument('files', nargs='+')
+    parser.add_argument('--db_name', '-n', type=str)
+    #parser.add_argument('-opt', '--optimise', action='store_true')
+    parser.add_argument('-dir','--directory',type=str)
     args = parser.parse_args()
 
-    main(args.calculation_name, args.files)
+    main(args.calculation_name, args.files, args.db_name, args.directory)

b34531e70a2ebf2a81ba8bd69df4b972de6f540f
ThorKongstad <thorkongstadmadsn@hotmail.com>
2024-06-28 13:31:21 +0200

    eq correction

--- compare_neb_images.py
+++ compare_neb_images.py
@@ -44,15 +44,15 @@ def main(neb_image_1: str, slab_for_1: str, neb_image_2: str, slab_for_2: str):
     neb_2_ens = bytes_to_object(neb_2_pd.iloc[0].loc['_data']).get('ensemble_en')
     slab_2_ens = bytes_to_object(slab_2_pd.iloc[0].loc['_data']).get('ensemble_en')
 
-    energy_ensemble = [n_2 + s_2 - n_1 - s_1 for n_1, s_1, n_2, s_2 in zip(neb_1_ens, slab_1_ens, neb_2_ens, slab_2_ens)]
+    energy_ensemble = [n_2 + s_1 - n_1 - s_2 for n_1, s_1, n_2, s_2 in zip(neb_1_ens, slab_1_ens, neb_2_ens, slab_2_ens)]
 
     print(f'energy difference between image 1 and the reference 2 is: {energy_diff:.3f} +- {sd(energy_ensemble, mean_value=mean(energy_ensemble)):.3f} eV')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('neb_image_1', help='format: db_path.db@row_nr')
-    parser.add_argument('slab_for_1', help='format: db_path.db@row_nr')
+    parser.add_argument('neb_image_1', help='reference format: db_path.db@row_nr')
+    parser.add_argument('slab_for_1', help='reference format: db_path.db@row_nr')
     parser.add_argument('neb_image_2', help='format: db_path.db@row_nr')
     parser.add_argument('slab_for_2', help='format: db_path.db@row_nr')
     args = parser.parse_args()
